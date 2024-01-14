6f2cd5b339e4a35d388b345967383fe947286a08
quang vu <quangvu37781@gmail.com>
2024-01-14 17:42:07 +0700

    up: reponsive banner 80%

--- src/pages/HomePage/components/BannerSection/index.jsx
+++ src/pages/HomePage/components/BannerSection/index.jsx
@@ -12,18 +12,18 @@ const BannerSection = () => {
         className="absolute top-0 left-0 bottom-0 w-full h-full z-[-1]"
       />
       <div className="flex flex-col items-center">
-        <span className="font-great-vibes text-white text-2xl tracking-wider capitalize">
+        <span className="font-great-vibes text-white text-lg lg:text-2xl tracking-wider capitalize">
           Best sushi in town
         </span>
-        <span className="font-cinzel-serif leading-normal text-[#F3D382] text-6xl font-bold text-center max-w-[49.75rem]">
+        <span className="font-cinzel-serif leading-normal text-[#F3D382] text-3xl lg:text-6xl font-bold text-center max-w-[25rem] lg:max-w-[49.75rem]">
           Taste the rich flavor of high quality sushi
         </span>
-        <span className="text-[#D9D9D9] text-center text-xl leading-normal max-w-[23rem]">
+        <span className="text-[#D9D9D9] text-center text-lg mt-4 lg:text-xl leading-normal max-w-[23rem]">
           We only use the five star quality for our menu, come and get the
           richness in every food we serve.
         </span>
       </div>
-      <Button className="bg-[#F5BE32] py-3 px-10 mt-[3.06rem] uppercase font-cormorant-serif text-xl text-black font-semibold hover:opacity-70">
+      <Button className="bg-[#F5BE32] py-3 px-10 mt-[3rem] uppercase font-cormorant-serif text-xl text-black font-semibold hover:opacity-70">
         Go to menu
       </Button>
     </div>
